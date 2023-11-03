b9c053d3a90d924ea02b4df6d331bf1a18ad8ed6
nqh00 <76725656+nqh00@users.noreply.github.com>
2023-11-03 04:57:11 +0100

    Optimize rclone VFS cache

--- utils/rclone_realdebrid.sh
+++ utils/rclone_realdebrid.sh
@@ -5,4 +5,4 @@ if [ -z "$1" ]; then
   exit 1
 fi
 
-nohup rclone copy real-debrid:links "onedrive:$1" --filter-from ~/filter-real-debrid.txt  --transfers 10 --checkers 20 --bwlimit 0 --onedrive-chunk-size 20M --fast-list -v --retries 5 > ~/real-debrid.log 2>&1 &
+nohup rclone copy real-debrid:links "trois:$1" --filter-from ~/filter-real-debrid.txt  --transfers 2 --checkers 2 --multi-thread-streams 2  --multi-thread-cutoff 100M --onedrive-chunk-size 100M --bwlimit 0 --fast-list -v --retries 5 > ~/real-debrid.log 2>&1 &
